09e9c276d3090f85b10de638291f88689fa36fcb
phamthucvietanh <23020514@vnu.edu.vn>
2024-09-25 00:38:34 +0700

    add hw1

---
+++ HW1/Introdution.java
@@ -0,0 +1,14 @@
+public class Introdution {
+    public static void main(String[] args) {
+        System.out.println("PHAM THUC VIET ANH\t23020514\tK68IS\tphamthucvietanh\t23020514@vnu.edu.vn");
+        System.out.println("9 bottles of beer on the wall, 9 bottles of beer.\r\n" + //
+                        "Take one down, pass it around, \r\n" + //
+                        "8 bottles of beer on the wall, 8 bottles of beer.\r\n" + //
+                        "Take one down, pass it around,\r\n" + //
+                        "...\r\n" + //
+                        "1 bottle of beer on the wall, 1 bottle of beer.\r\n" + //
+                        "Take one down, pass it around,\r\n" + //
+                        "No more bottles of beer on the wall.\r\n" + //
+                        "");
+    }
+}
